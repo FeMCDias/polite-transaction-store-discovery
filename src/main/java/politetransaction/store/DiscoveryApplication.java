777ae57474eb43652da4bd279701ba0edc8074fa
FeMCDias <femcdias@gmail.com>
2024-02-28 10:02:43 -0300

    feat: application file

--- src/main/java/politetransaction/store/DiscoveryApplication.java
+++ src/main/java/politetransaction/store/DiscoveryApplication.java
@@ -1,5 +1,14 @@
-package main.java.politetransaction.store;
+package politetransaction.store;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryApplication {
-    
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
 }
